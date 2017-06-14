572344fc2cb329142e4595edf1192f24122fb6be
DaniMontani <dnmontani@hotmail.com>
2017-06-13 22:17:49 -0300

    aula de revisao antes do feriado. Ve se estuda mulher!!!!

--- src/Conta.java
+++ src/Conta.java
@@ -1,15 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Conta {
 	double saldo;
 	String titular;
 	int numero;
-	
-	//Construtor da classe
-	public Conta(double saldo, String nome, int numero){
+
+	// Construtor da classe
+	public Conta(double saldo, String nome, int numero) {
 		this.saldo = saldo;
 		this.numero = numero;
 		this.titular = nome;
-		
+
 	}
 
 	public int debitar(double valor) {
@@ -18,7 +20,7 @@ public class Conta {
 			System.out.println("valor debitado na conta " + titular);
 			return 1;
 		} else {
-			System.out.println("Saldo Insuficiente na conta "+titular);
+			System.out.println("Saldo Insuficiente na conta " + titular);
 			return 0;
 		}
 	}
@@ -26,5 +28,25 @@ public class Conta {
 	public void creditar(double valor) {
 		saldo = saldo + valor;
 		System.out.println("valor creditado. Novo saldo " + saldo + " da conta " + titular);
+
+	}
+
+	public void salvar(String caminho) {
+		String end = caminho + "/Conta" + numero + ".txt";
+		try {
+			FileWriter arquivo = new FileWriter(end);
+			BufferedWriter escrita = new BufferedWriter(arquivo);
+			escrita.write(String.valueOf(saldo));
+			escrita.newLine();
+			escrita.write(String.valueOf(numero));
+			escrita.newLine();
+			//escrita.write(extrato.toString());
+			//escrita.newLine();
+			escrita.close();
+			arquivo.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
+		//escreveArquivo();
+		//leArquivo();
+		Conta caixa2 = new Conta(20000, "Aecio",12344);
+		caixa2.creditar(100);
+		caixa2.debitar(10200);
+		caixa2.creditar(120);
+		caixa2.debitar(100);
+		caixa2.creditar(280);
+		caixa2.creditar(5000);
+		caixa2.debitar(800);
+		caixa2.creditar(1400);
+		caixa2.salvar("d://");
+	}
+	
 
-		//ArrayList:<objeto> usuario = new ArrayList<objeto> usuario();
-		  ArrayList<String> usuario= new ArrayList<String>();
-		  Scanner Scan=new Scanner(System.in);
-		  String Fulano = Scan.nextLine(); 
-		  while(!Fulano.equals("sair")){
-			  usuario.add(Fulano);
-			  Fulano = Scan.nextLine();
-		  }
-		  for(int i=0;i<usuario.size();i++){
-			  System.out.println(usuario.get(i));
-		  }
-		  
-		  
+	public static void escreveArquivo() {
+		String end = "d://teste.txt";
+
+		try {
+			FileWriter file = new FileWriter(end);
+			BufferedWriter objEscrita = new BufferedWriter(file);
+			// PrintWriter escreve = new PrintWriter(file);
+			// escreve.print("Escrevi no arquivo");
+
+			objEscrita.write("Esse texto está sendo inserido");
+			objEscrita.newLine();
+			objEscrita.write("Atirei o pau no gato");
+			objEscrita.newLine();
+			objEscrita.write("Mas o gato não morreu");
+			objEscrita.newLine();
+			objEscrita.write("Dona Xica");
+			objEscrita.flush();
+			objEscrita.close();
+			file.close();
+
+			System.out.println("Rodou");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 
+	public static void leArquivo() {
+		String end = "d://teste.txt";
+		
+		try {
+			FileReader arquivo = new FileReader(end);
+			BufferedReader leitura = new BufferedReader(arquivo);
+			String linha = leitura.readLine();
+			while(linha !=null){
+				System.out.println(linha);
+				linha=leitura.readLine();
+			}	
+			leitura.close();
+			arquivo.close();
+		} catch (Exception e) {e.toString();
+			System.out.println(e.toString());
+
+		}
+	}
 }
